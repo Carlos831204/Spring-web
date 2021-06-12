d225b5042c1de5506ecc3e8bbaa195997099da60
DANIEL PEDRAZA <dapedraza@unbosque.edu.co>
2021-06-12 15:44:26 -0500

    Validacion web spring

---
+++ src/test/java/com/example/demo/DemoWebApplicationTests.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import com.example.demo.controller.DemoController;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class DemoWebApplicationTests {
+	@Autowired
+	private DemoController controller;
+	
+	@Test
+	void contextLoads() {
+		assertThat(controller).isNotNull();
+	
+	}
+
+}
