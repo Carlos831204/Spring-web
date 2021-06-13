16905f24603cb0a23ddf632a096e6877bf0cdefd
DANIEL PEDRAZA <dapedraza@unbosque.edu.co>
2021-06-12 22:33:06 -0500

    Proyecto web2 spring

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -1,16 +1,34 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.example.demo.model.Persona;
+import com.example.demo.repo.IPersonaRepo;
+
 @Controller
 public class DemoController {
-	@GetMapping("/greeting")
-	public String greeting(@RequestParam(name="pais", required=false, defaultValue="Mundo") String name, Model model) {
-		model.addAttribute("pais",name);
-		return "greeting";
+	
+	@Autowired
+	private IPersonaRepo repo;
+	
+	@GetMapping("/saludo")
+	public String saludo(@RequestParam(name="nombre", required=false, defaultValue="Mundo") String nombre, Model modelo) {
+		Persona p = new Persona();
+		p.setIdPersona(2);
+		p.setNombre("Camila");
+		repo.save(p);
+		modelo.addAttribute("nombre",nombre);
+		return "saludo";
+	}
+	
+	@GetMapping("/listar")
+	public String lista(Model modelo) {
+		modelo.addAttribute("personas", repo.findAll());
+		return "saludo";
 	}
 
 }
